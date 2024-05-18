ca112d2b49a737151c0560e943ff65a230268d62
Martino Phoenix Võ <phuong.vht.0504@gmail.com>
2024-05-18 16:02:35 +0700

    Remove all files from the repository.

--- DAL_BBMS/DBConnect.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Data.SqlClient;
-
-namespace DAL_BBMS
-{
-    public class DBConnect
-    {
-        protected SqlConnection _conn = new SqlConnection("Data Source=PHOENIX_PC;Initial Catalog=BBMS;Integrated Security=True");
-    }
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# BloodBankManagementSystem
-This project will be used to create and use a blood bank management system (BBMS).
